d7b874814cc96dd8bf74710e2244ef8bf40a8055
Amanda Xiao <amanda64@email.unc.edu>
2024-02-25 04:43:03 -0500

    Add final design touches

--- backend/data.py
+++ backend/data.py
@@ -15,9 +15,9 @@ class Data:
         # Populating Student Union Bathroom Data
         Union = Building("Student Union", 35.9097, -79.0483)
         third_floor = Bathroom("Third Floor", True, True, False, False, True, False, False)
-        near_alpine = Bathroom("Across from Alpine", True, True, False, False, True, False, False)
-        auditorium = Bathroom("Near Auditorium", True, True, False, False, True, False, False)
-        bottom_floor = Bathroom("Bottom Floor", True, True, False, False, True, False, False)
+        near_alpine = Bathroom("Across from Alpine", True, False, False, False, True, False, True)
+        auditorium = Bathroom("Near Auditorium", True, True, False, True, True, False, False)
+        bottom_floor = Bathroom("Bottom Floor", False, True, False, False, True, False, False)
         Union.addBathroom(third_floor)
         Union.addBathroom(near_alpine)
         Union.addBathroom(auditorium)
@@ -25,7 +25,7 @@ class Data:
 
         # Populating Campus Y Bathroom Data
         CampusY = Building("Campus Y", 35.9114, -79.0512)
-        meantime = Bathroom("Near Meantime", True, True, False, False, True, False, False)
+        meantime = Bathroom("Near Meantime", False, False, False, False, True, True, True)
         second_floor = Bathroom("Second Floor", True, True, False, False, True, False, False)
         CampusY.addBathroom(meantime)
         CampusY.addBathroom(second_floor)
